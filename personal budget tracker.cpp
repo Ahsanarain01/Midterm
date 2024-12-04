9fe09f65a5b8f8d50d056e2e990e0b3a1914764d
Ahsanarain01 <mahsanarain1154@gmail.com>
2024-12-04 14:24:32 +0500

    Done

---
+++ personal budget tracker.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int income,expense,balance;
+	cout<<"Enter your income:" << endl;
+	cin >> income;
+	
+	if(expense){
+		cout << "Enter your expense:" <<  endl ;
+		cin >> expense;
+			if(balance){
+				cout << "Your balance is:" << income-expense << endl;
+			
+			if(){
+				cout << "Exit" <, endl;
+		 }
+			 }
+	}
+	
+	return 0;
+}
